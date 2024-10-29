441918bc2a99f67989fa1a6317783e0c98b3faf6
noelia <Usuario@DESKTOP-RU6Q6T0.>
2024-10-28 22:51:47 -0300

    Se crea el archivo para la pantalla register

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/login/ui/RegisterScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/login/ui/RegisterScreen.kt
@@ -1,7 +1,6 @@
 package ar.edu.unlam.mobile.scaffolding.ui.login.ui
+
 import android.annotation.SuppressLint
-import android.provider.CalendarContract.Colors
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,63 +11,63 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import java.lang.Compiler.disable
 
 
 @Composable
-fun loginScreen(){
+fun loginScreen(viewModel: RegisterScreenViewModel) {
     Box(Modifier.fillMaxSize().padding(16.dp)){
-        Login(Modifier.align(Alignment.Center))
+        Register(Modifier.align(Alignment.Center),viewModel)
     }
 }
 
 @Composable
-fun Login(modifier: Modifier) {
+fun Register(modifier: Modifier, viewModel: RegisterScreenViewModel) {
+val email:String by viewModel.email.observeAsState(initial="")
+    val password:String by viewModel.password.observeAsState(initial = "")
+val registerEnable:Boolean by viewModel.registerEnable.observeAsState(initial = false)
+val nameUser:String by viewModel.nameUser.observeAsState(initial="")
+
 Column (modifier=modifier){
  //HeaderImage(modifier.align(Alignment.CenterHorizontally))
   Spacer(modifier = modifier.padding(16.dp))
-    NameUserField()
+    NameUserField(nameUser){viewModel.onRegisterChanged(it,email,password)}
     Spacer(modifier = modifier.padding(16.dp))
-    EmailField()
+    EmailField(email){viewModel.onRegisterChanged(it,password,nameUser)}
     Spacer(modifier = modifier.padding(16.dp))
-    PassWordField()
+    PassWordField(password){viewModel.onRegisterChanged(email,nameUser,it)}
     Spacer(modifier = modifier.padding(16.dp))
-    PassWordField()
-    RegisterBotton()
+    PassWordField(password){viewModel.onRegisterChanged(email,nameUser,it)}
+    RegisterBotton(registerEnable){viewModel.onRegisterSelected()}
 
 }
 }
-@Preview
+
 @Composable
-fun RegisterBotton() {
-    Button(onClick = {},
-        modifier=Modifier.fillMaxWidth().height(40.dp)),
-    Color = ButtonDefaults.buttonColors(
-        backgroundColor=Color(0xFFFF4303),
-    disableBackgroundColor=Color(0xFFF78058),
-    contentColor= Color.Blue
-    disableContentColor=Color.Cyan
-    ){
+fun RegisterBotton(registerEnable: Boolean, onRegisterSelected: () -> Unit) {
+    Button(onClick = {onRegisterSelected()} ,
+        modifier=Modifier.fillMaxWidth().height(40.dp),
+        enabled=registerEnable
+        )
+    {
         Text(text="Registrarse")
     }
 }
 
-@Preview
+@SuppressLint("UnrememberedMutableInteractionSource")
+
 @Composable
-fun NameUserField() {
-    TextField(value = "", onValueChange = {},
+fun NameUserField(nameUser: String,onTextFieldChange: (String)  -> Unit) {
+    TextField(value = nameUser, onValueChange = {onTextFieldChange(it)},
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text(text="Nombre de Usuario") },
         label = { Text("Nombre de Usuario") },
@@ -80,11 +79,11 @@ fun NameUserField() {
     )
 }
 
-@Preview
+
 @SuppressLint("UnrememberedMutableInteractionSource")
 @Composable
-fun PassWordField() {
-    TextField(value = "", onValueChange = {},
+fun PassWordField(password:String,onTextFieldChange: (String) -> Unit) {
+    TextField(value = password, onValueChange = {onTextFieldChange(it)},
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text(text="Password") },
         label = { Text("Password") },
@@ -96,14 +95,14 @@ fun PassWordField() {
     )
 
 }
-@Preview
+
 @SuppressLint("UnrememberedMutableInteractionSource")
 @Composable
-fun EmailField() {
-    TextField(value = "", onValueChange = {},
+fun EmailField(email:String, onTextFieldChange: (String) -> Unit)  {
+
+    TextField(value = email, onValueChange = {onTextFieldChange(it)},
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text(text="Email") },
-
         singleLine = true,
         maxLines = 1,
         keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
