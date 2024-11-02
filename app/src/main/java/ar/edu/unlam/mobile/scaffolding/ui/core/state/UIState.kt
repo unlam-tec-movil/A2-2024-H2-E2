19710e4427e878fc49050570d493a3ac7fe436b1
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-02 20:23:28 -0300

    Implementación de UIState para estados genéricos

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/state/UIState.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/state/UIState.kt
@@ -1,5 +1,28 @@
 package ar.edu.unlam.mobile.scaffolding.ui.core.state
 
-sealed interface UIState<out T> {
-    // TODO: Implementar estados Loading/Success/Error
+sealed class UIState<out T> {
+    data object Loading : UIState<Nothing>()
+    data class Success<T>(val data: T) : UIState<T>()
+    data class Error(val message: String) : UIState<Nothing>()
+}
+
+fun <T> UIState<T>.onSuccess(action: (T) -> Unit): UIState<T> {
+    if (this is UIState.Success){
+        action(data)
+    }
+    return this
+}
+
+fun <T> UIState<T>.onError(action: (String) -> Unit): UIState<T> {
+    if (this is UIState.Error){
+        action(message)
+    }
+    return this
+}
+
+fun <T> UIState<T>.onLoading(action: () -> Unit): UIState<T> {
+    if (this is UIState.Loading) {
+        action()
+    }
+    return this
 }
