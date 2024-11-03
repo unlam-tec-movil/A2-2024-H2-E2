f827f5a3e37f8d67ab09adc65aa91c2a85fc813e
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-03 20:39:02 -0300

    corrección de espacios

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/state/UIState.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/state/UIState.kt
@@ -2,7 +2,7 @@ package ar.edu.unlam.mobile.scaffolding.ui.core.state
 
 sealed class UIState<out T> {
     data object Loading : UIState<Nothing>()
-    
+
     data class Success<T>(val data: T) : UIState<T>()
 
     data class Error(val message: String) : UIState<Nothing>()
