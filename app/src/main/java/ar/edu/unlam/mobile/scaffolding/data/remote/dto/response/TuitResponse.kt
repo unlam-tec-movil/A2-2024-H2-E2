2c0d4943db6eef7781e52bd62afbc5cb74093b47
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 20:07:51 -0300

    Implementación TuitResponse

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/TuitResponse.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/TuitResponse.kt
@@ -1,9 +1,11 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class TuitResponse(
-    val id: Int,
-    val message: String,
-    val author: String,
-    val likes: Int,
-    val liked: Boolean,
+    @SerializedName("id") val id: Int,
+    @SerializedName("message") val message: String,
+    @SerializedName("author") val author: String,
+    @SerializedName("likes") val likes: Int,
+    @SerializedName("liked") val liked: Boolean,
 )
