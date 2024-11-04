6b73e8967883684319a2e77339a99d5c56ae9152
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-03 21:24:22 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/component/error/ErrorView.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/core/component/error/ErrorView.kt
@@ -28,16 +28,14 @@ fun ErrorView(
     isRetrying: Boolean = false,
 ) {
     Card(
-        modifier = Modifier
-            .padding(16.dp)
+        modifier = Modifier.padding(16.dp)
             .fillMaxWidth(),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.errorContainer,
         ),
     ) {
         Column(
-            modifier = Modifier
-                .padding(16.dp)
+            modifier = Modifier.padding(16.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
