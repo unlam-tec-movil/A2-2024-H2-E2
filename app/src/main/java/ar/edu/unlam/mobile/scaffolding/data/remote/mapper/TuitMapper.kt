08eaf39802a73c2d7195412630efa05c8200ad59
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 20:14:59 -0300

    Implementación de TuitMapper para transformar de tipo TuitResponse a tipo Tuit

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/TuitMapper.kt
@@ -0,0 +1,26 @@
+package ar.edu.unlam.mobile.scaffolding.data.remote.mapper
+
+import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.TuitResponse
+import ar.edu.unlam.mobile.scaffolding.domain.model.Tuit
+import javax.inject.Inject
+
+class TuitMapper @Inject constructor() {
+
+    private fun toDomain(response: TuitResponse): Tuit {
+        return Tuit(
+            id = response.id,
+            message = response.message,
+            author = response.author,
+            likes = response.likes,
+            liked = response.liked,
+            parentId = 0,
+            avatarUrl = "https://ui-avatars.com/api/?name=${response.author.replace(" ", "+")}",
+            date = System.currentTimeMillis().toString(),
+            replies = 0
+        )
+    }
+
+    fun toDomainList(responses: List<TuitResponse>): List<Tuit> {
+        return responses.map { toDomain(it) }
+    }
+}
