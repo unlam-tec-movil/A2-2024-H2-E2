27d9a8c3031282ee76a066646825c17bcf8beb32
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 18:08:25 -0300

    Implementación de TuitResponse

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/TuitResponse.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/TuitResponse.kt
@@ -1,9 +1,9 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.response
 
-/*data class TuitResponse(
-    // TODO: Implementar campos
-)*/
-
-class TuitResponse {
-    // TODO: borrar esta clase y usar la de arriba cuando se implemente
-}
+data class TuitResponse(
+    val id: Int,
+    val message: String,
+    val author: String,
+    val likes: Int,
+    val liked: Boolean,
+)
