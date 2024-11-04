72ce04752641ee79b8db87cc6fe5c1e91984c552
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 16:23:56 -0300

    Implementación TuitApi de data/api

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/TuitApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/TuitApi.kt
@@ -1,5 +1,26 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.api
 
+import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.TuitResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface TuitApi {
-    // TODO: Implementar endpoints de tuits
+    @GET("/api/v1/me/feed")
+    suspend fun getFeed(): List<TuitResponse>
+
+    @GET("/api/v1/me/tuits/{id}")
+    suspend fun getTuitById(@Path("id") id: String): TuitResponse
+
+    @POST("/api/v1/me/tuits")
+    suspend fun createTuit(@Body message: String): TuitResponse
+
+    @POST("/api/v1/me/tuits/{id}/likes")
+    suspend fun likeTuit(@Path("id") tuitId: String)
+
+    @DELETE("/api/v1/me/tuits/{id}/likes")
+    suspend fun unlikeTuit(@Path("id") tuitId: String)
+
 }
