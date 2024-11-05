ccef139cb0b8939ced31d6b0616ff34798e745ec
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 21:44:55 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/TuitMapper.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/TuitMapper.kt
@@ -7,19 +7,19 @@ import javax.inject.Inject
 class TuitMapper
     @Inject
     constructor() {
-    private fun toDomain(response: TuitResponse): Tuit {
-        return Tuit(
-            id = response.id,
-            message = response.message,
-            author = response.author,
-            likes = response.likes,
-            liked = response.liked,
-            parentId = 0,
-            avatarUrl = "https://ui-avatars.com/api/?name=${response.author.replace(" ", "+")}",
-            date = System.currentTimeMillis().toString(),
-            replies = 0,
-        )
-    }
+        private fun toDomain(response: TuitResponse): Tuit {
+            return Tuit(
+                id = response.id,
+                message = response.message,
+                author = response.author,
+                likes = response.likes,
+                liked = response.liked,
+                parentId = 0,
+                avatarUrl = "https://ui-avatars.com/api/?name=${response.author.replace(" ", "+")}",
+                date = System.currentTimeMillis().toString(),
+                replies = 0,
+            )
+        }
 
     fun toDomainList(responses: List<TuitResponse>): List<Tuit> {
         return responses.map { toDomain(it) }
