af4b751b70f852a013538f39f8539532d5a781c2
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-04 22:48:46 -0300

    Implementación RepositoryModule

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/RepositoryModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/RepositoryModule.kt
@@ -1,15 +1,23 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import ar.edu.unlam.mobile.scaffolding.data.remote.api.TuitApi
+import ar.edu.unlam.mobile.scaffolding.data.repository.RemoteTuitRepository
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.TuitRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RepositoryModule {
-    // TODO: Proveer RemoteTuitRepository
-    // TODO: Proveer StoredTuitRepository
-    // TODO: Proveer AuthenticationRepository
-    // TODO: Proveer ProfileRepository
-    // TODO: Proveer FavoriteUserRepository
+    @Provides
+    @Singleton
+    fun provideTuitRepository(
+        api: TuitApi,
+        mapper: TuitMapper,
+    ): TuitRepository {
+        return RemoteTuitRepository(api, mapper)
+    }
 }
