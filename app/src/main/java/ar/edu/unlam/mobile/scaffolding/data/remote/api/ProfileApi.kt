1bbb8412f7f599bec5a68f2e476636ae2f4ace32
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-06 14:54:45 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
@@ -2,8 +2,6 @@ package ar.edu.unlam.mobile.scaffolding.data.remote.api
 
 import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.ProfileResponse
 import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface ProfileApi {
     @GET("/api/v1/me/profile")
