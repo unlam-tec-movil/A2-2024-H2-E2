fb66a3d1f144210617990d910be3e12c9320e4dc
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-06 14:45:07 -0300

    Update ProfileApi.kt solo funcion getProfile

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
@@ -8,19 +8,4 @@ import retrofit2.http.Path
 interface ProfileApi {
     @GET("/api/v1/me/profile")
     suspend fun getProfile(): ProfileResponse
-
-    @POST("/api/v1/me/profile/{name}")
-    suspend fun updateProfileName(
-        @Path("name") newName: String,
-    ): ProfileResponse
-
-    @POST("/api/v1/me/profile/{avatarUrl}")
-    suspend fun updateProfileAvatar(
-        @Path("avatarUrl") newAvatarUrl: String,
-    ): ProfileResponse
-
-    @POST("/api/v1/me/profile/{email}")
-    suspend fun updateProfileEmail(
-        @Path("email") newEmail: String,
-    ): ProfileResponse
 }
