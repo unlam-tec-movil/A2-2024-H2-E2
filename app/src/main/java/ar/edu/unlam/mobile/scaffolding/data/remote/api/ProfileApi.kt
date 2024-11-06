9a74f7566a8b2def2dec4db401c4d049cf4059eb
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-06 12:38:02 -0300

    Agregado los endpoints de perfil en ProfileApi

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/api/ProfileApi.kt
@@ -1,5 +1,26 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.api
 
+import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.ProfileResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface ProfileApi {
-    // TODO: Implementar endpoints de perfil
+    @GET("/api/v1/me/profile")
+    suspend fun getProfile(): ProfileResponse
+
+    @POST("/api/v1/me/profile/{name}")
+    suspend fun updateProfileName(
+        @Path("name") newName: String,
+    ): ProfileResponse
+
+    @POST("/api/v1/me/profile/{avatarUrl}")
+    suspend fun updateProfileAvatar(
+        @Path("avatarUrl") newAvatarUrl: String,
+    ): ProfileResponse
+
+    @POST("/api/v1/me/profile/{email}")
+    suspend fun updateProfileEmail(
+        @Path("email") newEmail: String,
+    ): ProfileResponse
 }
