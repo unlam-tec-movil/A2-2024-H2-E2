02862394d3aba9201f60f3adffb50a92dcf8f766
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-06 18:53:04 -0300

    Se implementa metodo en ProfileRepository + ProfileMapper

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/ProfileRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/ProfileRepository.kt
@@ -1,12 +1,18 @@
 package ar.edu.unlam.mobile.scaffolding.data.repository
 
 import ar.edu.unlam.mobile.scaffolding.data.remote.api.ProfileApi
+import ar.edu.unlam.mobile.scaffolding.data.remote.mapper.ProfileMapper
+import ar.edu.unlam.mobile.scaffolding.domain.model.Profile
 import javax.inject.Inject
 
 class ProfileRepository
     @Inject
     constructor(
-        private val profileApi: ProfileApi,
+        private val api: ProfileApi,
+        private val mapper: ProfileMapper,
     ) {
-        // TODO: Implementar métodos de perfil
+        suspend fun getProfile(): Profile {
+            val response = api.getProfile()
+            return mapper.toDomain(response)
+        }
     }
