a876bc8d62c5cb423c73f202844ef3adcb47d65e
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-07 02:11:24 -0300

    Se implementa ProfileViewModel

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileViewModel.kt
@@ -1,9 +1,50 @@
 package ar.edu.unlam.mobile.scaffolding.ui.user.profile
 
 import androidx.lifecycle.ViewModel
-/*import dagger.hilt.android.lifecycle.HiltViewModel
-@HiltViewModel TODO: Descomentar cuando se implemente y bajar una linea el annotation*/
+import androidx.lifecycle.viewModelScope
+import ar.edu.unlam.mobile.scaffolding.data.repository.ProfileRepository
+import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileViewModel : ViewModel() {
-    // TODO: Implementar lógica de perfil
-}
+@HiltViewModel
+class ProfileViewModel
+    @Inject
+    constructor(
+        private val profileRepository: ProfileRepository,
+    ) : ViewModel() {
+        private val _state = MutableStateFlow(ProfileState())
+        val state = _state.asStateFlow()
+
+        init {
+            viewModelScope.launch {
+                try {
+                    _state.update {
+                        it.copy(
+                            profileState = UIState.Loading,
+                        )
+                    }
+                    val profile = profileRepository.getProfile()
+                    _state.update {
+                        it.copy(
+                            profileState =
+                                UIState.Success(profile),
+                        )
+                    }
+                } catch (e: Exception) {
+                    _state.update {
+                        it.copy(
+                            profileState =
+                                UIState.Error(
+                                    e.message ?: "Hubo un error",
+                                ),
+                        )
+                    }
+                }
+            }
+        }
+    }
