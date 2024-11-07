dbd3c4ef16de77865759a93a6d8a7908fa62bb90
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-07 12:50:37 -0300

    Update ProfileViewModel

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileViewModel.kt
@@ -2,12 +2,11 @@ package ar.edu.unlam.mobile.scaffolding.ui.user.profile
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import ar.edu.unlam.mobile.scaffolding.data.repository.ProfileRepository
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.profile.GetProfile
 import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,35 +14,28 @@ import javax.inject.Inject
 class ProfileViewModel
     @Inject
     constructor(
-        private val profileRepository: ProfileRepository,
+        private val getProfile: GetProfile,
     ) : ViewModel() {
         private val _state = MutableStateFlow(ProfileState())
         val state = _state.asStateFlow()
 
         init {
+            loadUserProfile()
+        }
+
+        private fun loadUserProfile() {
             viewModelScope.launch {
                 try {
-                    _state.update {
-                        it.copy(
-                            profileState = UIState.Loading,
+                    val profile = getProfile()
+                    _state.value =
+                        _state.value.copy(
+                            profileState = UIState.Success(profile),
                         )
-                    }
-                    val profile = profileRepository.getProfile()
-                    _state.update {
-                        it.copy(
-                            profileState =
-                                UIState.Success(profile),
-                        )
-                    }
                 } catch (e: Exception) {
-                    _state.update {
-                        it.copy(
-                            profileState =
-                                UIState.Error(
-                                    e.message ?: "Hubo un error",
-                                ),
+                    _state.value =
+                        _state.value.copy(
+                            profileState = UIState.Error(e.message ?: "Error al cargar el perfil"),
                         )
-                    }
                 }
             }
         }
