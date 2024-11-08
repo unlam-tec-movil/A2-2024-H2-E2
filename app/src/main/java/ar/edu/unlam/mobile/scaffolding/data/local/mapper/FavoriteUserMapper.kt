c602d348374ec2be8bc128679efaf6771fe013d1
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 01:51:08 -0300

    agregado de FavoriteUserMapper

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/mapper/FavoriteUserMapper.kt
@@ -0,0 +1,28 @@
+package ar.edu.unlam.mobile.scaffolding.data.local.mapper
+
+import ar.edu.unlam.mobile.scaffolding.data.local.entity.FavoriteUserEntity
+import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
+
+object FavoriteUserMapper {
+    fun mapToDomain(favoriteUserEntity: FavoriteUserEntity): FavoriteUser {
+        return FavoriteUser(
+            name = favoriteUserEntity.name,
+            avatarUrl = favoriteUserEntity.avatarUrl
+        )
+    }
+
+    fun mapToEntity(favoriteUser: FavoriteUser): FavoriteUserEntity {
+        return FavoriteUserEntity(
+            name = favoriteUser.name,
+            avatarUrl = favoriteUser.avatarUrl
+        )
+    }
+
+    fun mapToDomainList(favoriteUserEntities: List<FavoriteUserEntity>): List<FavoriteUser>{
+        return favoriteUserEntities.map { mapToDomain(it) }
+    }
+
+    fun mapToEntityList(favoriteUsers: List<FavoriteUser>): List<FavoriteUserEntity>{
+        return favoriteUsers.map { mapToEntity(it) }
+    }
+}
