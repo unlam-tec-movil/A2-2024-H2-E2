60dd1242bbd83fbb0f1d2e81b9db85c182cdd8a5
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 02:50:43 -0300

    implementacion metodo RemoveFavoriteUser

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/favorite/RemoveFavoriteUser.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/favorite/RemoveFavoriteUser.kt
@@ -1,5 +1,10 @@
 package ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite
 
-interface RemoveFavoriteUser {
-    // TODO: Método para eliminar usuario favorito
+import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
+
+data class RemoveFavoriteUser (private val favoriteUserRepository: FavoriteUserRepository) {
+    suspend operator fun invoke(user: FavoriteUser){
+        favoriteUserRepository.deleteFavoriteUser(user)
+    }
 }
