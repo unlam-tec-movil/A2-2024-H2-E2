83d5fd6dc4e3943f9837d6f25c4bd8dd343d60a3
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 03:00:54 -0300

    provide de GetFavoriteUsers, RemoveFavoriteUser y SaveFavoriteUser

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/UseCaseModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/UseCaseModule.kt
@@ -1,14 +1,35 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.GetFavoriteUsers
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.RemoveFavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.SaveFavoriteUser
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
-    // TODO: Proveer casos de uso de tuits
-    // TODO: Proveer casos de uso de autenticación
-    // TODO: Proveer casos de uso de perfil
-    // TODO: Proveer casos de uso de favoritos
+    @Provides
+    fun provideSaveFavoriteUser(
+        favoriteUserRepository: FavoriteUserRepository
+    ): SaveFavoriteUser {
+        return SaveFavoriteUser(favoriteUserRepository)
+    }
+
+    @Provides
+    fun provideRemoveFavoriteUser(
+        favoriteUserRepository: FavoriteUserRepository
+    ): RemoveFavoriteUser {
+        return RemoveFavoriteUser(favoriteUserRepository)
+    }
+
+    @Provides
+    fun provideGetFavoriteUsers(
+        favoriteUserRepository: FavoriteUserRepository
+    ): GetFavoriteUsers {
+        return GetFavoriteUsers(favoriteUserRepository)
+    }
 }
