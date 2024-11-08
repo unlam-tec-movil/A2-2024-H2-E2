1fe4ae345d136a79d13d998d604e7098ffbb7ad5
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 02:20:35 -0300

    correccion errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/mapper/FavoriteUserMapper.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/mapper/FavoriteUserMapper.kt
@@ -4,27 +4,28 @@ import ar.edu.unlam.mobile.scaffolding.data.local.entity.FavoriteUserEntity
 import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
 import javax.inject.Inject
 
-class FavoriteUserMapper @Inject constructor()
-{
-    fun mapToDomain(favoriteUserEntity: FavoriteUserEntity): FavoriteUser {
-        return FavoriteUser(
-            name = favoriteUserEntity.name,
-            avatarUrl = favoriteUserEntity.avatarUrl,
-        )
-    }
+class FavoriteUserMapper
+    @Inject
+    constructor() {
+        fun mapToDomain(favoriteUserEntity: FavoriteUserEntity): FavoriteUser {
+            return FavoriteUser(
+                name = favoriteUserEntity.name,
+                avatarUrl = favoriteUserEntity.avatarUrl,
+                )
+        }
 
-    fun mapToEntity(favoriteUser: FavoriteUser): FavoriteUserEntity {
-        return FavoriteUserEntity(
-            name = favoriteUser.name,
-            avatarUrl = favoriteUser.avatarUrl,
-        )
-    }
+        fun mapToEntity(favoriteUser: FavoriteUser): FavoriteUserEntity {
+            return FavoriteUserEntity(
+                name = favoriteUser.name,
+                avatarUrl = favoriteUser.avatarUrl,
+                )
+        }
 
-    fun mapToDomainList(favoriteUserEntities: List<FavoriteUserEntity>): List<FavoriteUser> {
-        return favoriteUserEntities.map { mapToDomain(it) }
-    }
+        fun mapToDomainList(favoriteUserEntities: List<FavoriteUserEntity>): List<FavoriteUser> {
+            return favoriteUserEntities.map { mapToDomain(it) }
+        }
 
-    fun mapToEntityList(favoriteUsers: List<FavoriteUser>): List<FavoriteUserEntity> {
-        return favoriteUsers.map { mapToEntity(it) }
-    }
+        fun mapToEntityList(favoriteUsers: List<FavoriteUser>): List<FavoriteUserEntity> {
+            return favoriteUsers.map { mapToEntity(it) }
+        }
 }
