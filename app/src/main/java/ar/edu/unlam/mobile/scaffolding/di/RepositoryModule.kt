ae05c59c724abbf1f726f32db50650d5bd12d6fe
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 02:57:10 -0300

    provide de FavoriteUserRepository

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/RepositoryModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import ar.edu.unlam.mobile.scaffolding.data.local.dao.FavoriteUserDao
 import ar.edu.unlam.mobile.scaffolding.data.remote.api.TuitApi
 import ar.edu.unlam.mobile.scaffolding.data.remote.mapper.TuitMapper
 import ar.edu.unlam.mobile.scaffolding.data.repository.RemoteTuitRepository
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
 import ar.edu.unlam.mobile.scaffolding.domain.port.repository.TuitRepository
 import dagger.Module
 import dagger.Provides
@@ -20,5 +22,14 @@ object RepositoryModule {
         mapper: TuitMapper,
     ): TuitRepository {
         return RemoteTuitRepository(api, mapper)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavoriteUserRepository(
+        dao: FavoriteUserDao,
+        mapper: FavoriteUserMapper,
+    ): FavoriteUserRepository {
+        return FavoriteUserRepositoryImpl(dao, mapper)
     }
 }
