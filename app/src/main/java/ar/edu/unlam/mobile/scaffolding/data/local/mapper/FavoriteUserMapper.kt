27d064c0903f3ea89a9fa27ef70d5a69aad45d8c
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 02:13:42 -0300

    FavoriteUserMapper debe ser una clase, no un object

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/mapper/FavoriteUserMapper.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/local/mapper/FavoriteUserMapper.kt
@@ -2,27 +2,29 @@ package ar.edu.unlam.mobile.scaffolding.data.local.mapper
 
 import ar.edu.unlam.mobile.scaffolding.data.local.entity.FavoriteUserEntity
 import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
+import javax.inject.Inject
 
-object FavoriteUserMapper {
+class FavoriteUserMapper @Inject constructor()
+{
     fun mapToDomain(favoriteUserEntity: FavoriteUserEntity): FavoriteUser {
         return FavoriteUser(
             name = favoriteUserEntity.name,
-            avatarUrl = favoriteUserEntity.avatarUrl
+            avatarUrl = favoriteUserEntity.avatarUrl,
         )
     }
 
     fun mapToEntity(favoriteUser: FavoriteUser): FavoriteUserEntity {
         return FavoriteUserEntity(
             name = favoriteUser.name,
-            avatarUrl = favoriteUser.avatarUrl
+            avatarUrl = favoriteUser.avatarUrl,
         )
     }
 
-    fun mapToDomainList(favoriteUserEntities: List<FavoriteUserEntity>): List<FavoriteUser>{
+    fun mapToDomainList(favoriteUserEntities: List<FavoriteUserEntity>): List<FavoriteUser> {
         return favoriteUserEntities.map { mapToDomain(it) }
     }
 
-    fun mapToEntityList(favoriteUsers: List<FavoriteUser>): List<FavoriteUserEntity>{
+    fun mapToEntityList(favoriteUsers: List<FavoriteUser>): List<FavoriteUserEntity> {
         return favoriteUsers.map { mapToEntity(it) }
     }
 }
