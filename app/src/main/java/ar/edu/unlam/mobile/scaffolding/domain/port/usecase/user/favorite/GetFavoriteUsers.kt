7e621e38442646959e524608655815034d8c827b
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 02:50:13 -0300

    implementacion metodo GetFavoriteUsers

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/favorite/GetFavoriteUsers.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/favorite/GetFavoriteUsers.kt
@@ -1,5 +1,10 @@
 package ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite
 
-interface GetFavoriteUsers {
-    // TODO: Método para obtener usuarios favoritos
+import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
+
+data class GetFavoriteUsers (private val favoriteUserRepository: FavoriteUserRepository) {
+    suspend operator fun invoke(): List<FavoriteUser> {
+        return favoriteUserRepository.getFavoriteUsers()
+    }
 }
