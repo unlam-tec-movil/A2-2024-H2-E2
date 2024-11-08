053fa9e917f68b0f7d8647be39ab9d2c960d6a5e
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-08 01:59:46 -0300

    implementacion de metodos para usuarios favoritos

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/FavoriteUserRepositoryImpl.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/FavoriteUserRepositoryImpl.kt
@@ -1,12 +1,25 @@
 package ar.edu.unlam.mobile.scaffolding.data.repository
 
 import ar.edu.unlam.mobile.scaffolding.data.local.dao.FavoriteUserDao
+import ar.edu.unlam.mobile.scaffolding.data.local.mapper.FavoriteUserMapper
+import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
 import javax.inject.Inject
 
 class FavoriteUserRepositoryImpl
     @Inject
     constructor(
         private val favoriteUserDao: FavoriteUserDao,
-    ) {
-        // TODO: Implementar métodos para usuarios favoritos
+    ) : FavoriteUserRepository{
+    override suspend fun savefavoriteUser(user: FavoriteUser) {
+        favoriteUserDao.saveFavoriteUser(FavoriteUserMapper.mapToEntity(user))
+    }
+
+    override suspend fun getFavoriteUsers(): List<FavoriteUser> {
+        return FavoriteUserMapper.mapToDomainList(favoriteUserDao.getFavoriteUsers())
+    }
+
+    override suspend fun deleteFavoriteUser(user: FavoriteUser) {
+        favoriteUserDao.deleteFavoriteUser(FavoriteUserMapper.mapToEntity(user))
+    }
     }
