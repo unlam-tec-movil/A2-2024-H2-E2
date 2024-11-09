1144491014867d9e8041ecab68a03a70e8abdff0
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 18:36:28 -0300

    agregada funcion de registro

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
@@ -3,6 +3,8 @@ package ar.edu.unlam.mobile.scaffolding.domain.port.repository
 interface AuthRepository {
     suspend fun login(credentials: LoginCredentials): Result<AuthToken>
 
+    suspend fun register(name: String, email: String, password: String): Result<AuthToken>
+
     fun saveToken(token: AuthToken)
 
     fun getToken(): AuthToken?
