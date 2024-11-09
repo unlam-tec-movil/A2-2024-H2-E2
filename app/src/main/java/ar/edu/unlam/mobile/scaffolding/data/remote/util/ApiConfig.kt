62619e0aa58d7158f3590b472431c74e5d647c96
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 12:27:17 -0300

    se suman nuevas constantes al api config

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/util/ApiConfig.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/util/ApiConfig.kt
@@ -1,7 +1,9 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.util
 
 object ApiConfig {
-    const val BASE_URL = "http://localhost:8000"
+    const val API_BASE_URL = "https://tuiter.fragua.com.ar"
+    const val LOCALHOST_BASE_URL = "http://localhost:8000"
     const val APPLICATION_TOKEN_HEADER = "Application-Token"
+    const val USER_TOKEN_HEADER = "Authorization"
 }
 // Ver implementación de Retrofit, OkHttpClient, AuthInterceptor
