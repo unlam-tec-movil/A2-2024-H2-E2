b81b3adc2883749ab86995e6f918e43f62749859
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 16:13:48 -0300

    Agregar los @SerializedName

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LoginRequest.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LoginRequest.kt
@@ -1,6 +1,8 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.request
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginRequest(
-    val email: String,
-    val password: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
 )
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/UserResponse.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/UserResponse.kt
@@ -1,7 +1,9 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.response
 
+import com.google.gson.annotations.SerializedName
+
 data class UserResponse(
-    val name: String,
-    val email: String,
-    val token: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("token") val token: String,
 )
