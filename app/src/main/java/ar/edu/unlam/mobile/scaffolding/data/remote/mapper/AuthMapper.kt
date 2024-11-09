1039d143291e3ca7eccb3b2af0b7642ba80cd1f7
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 15:57:34 -0300

    Mapper de la autenticación

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/AuthMapper.kt
@@ -0,0 +1,23 @@
+package ar.edu.unlam.mobile.scaffolding.data.remote.mapper
+
+import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.UserResponse
+import ar.edu.unlam.mobile.scaffolding.domain.model.User
+import javax.inject.Inject
+
+class AuthMapper @Inject constructor() {
+
+    fun toUser(response: UserResponse) = User(
+        name = response.name,
+        email = response.email,
+        avatarUrl = response.avatar_url,
+    )
+
+    fun toAuthToken(response: UserResponse) = AuthToken(
+        token = response.token,
+    )
+
+    fun toLoginCredentials(email: String, password: String) = LoginCredentials(
+        email = email,
+        password = password,
+    )
+}
