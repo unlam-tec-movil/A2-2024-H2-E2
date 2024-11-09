0d7a57e40a27419905e081060cdb44fb182913cc
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 16:17:24 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/AuthMapper.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/AuthMapper.kt
@@ -7,19 +7,19 @@ import javax.inject.Inject
 class AuthMapper
     @Inject
     constructor() {
-
-        fun toUser(response: UserResponse) = User(
-            name = response.name,
-            email = response.email,
-            avatarUrl = response.avatar_url,
-        )
+        fun toUser(response: UserResponse) =
+            User(
+                name = response.name,
+                email = response.email,
+                avatarUrl = response.avatar_url,
+            )
 
         fun toAuthToken(response: UserResponse) = AuthToken(
-            token = response.token,
+                token = response.token,
         )
 
         fun toLoginCredentials(email: String, password: String) = LoginCredentials(
-            email = email,
-            password = password,
+                email = email,
+                password = password,
         )
     }
