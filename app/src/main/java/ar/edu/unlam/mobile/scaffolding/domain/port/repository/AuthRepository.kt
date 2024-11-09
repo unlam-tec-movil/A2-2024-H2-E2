ad7ce3c72c9d854179e62cafb3099f9b460142c0
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 15:36:35 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
@@ -2,7 +2,10 @@ package ar.edu.unlam.mobile.scaffolding.domain.port.repository
 
 interface AuthRepository {
     suspend fun login(credentials: LoginCredentials): Result<AuthToken>
+
     fun saveToken(token: AuthToken)
+
     fun getToken(): AuthToken?
+
     fun clearToken()
 }
