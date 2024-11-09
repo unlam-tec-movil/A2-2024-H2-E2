d664c2395d36bb599a270eee96b6b3b1c81c3a39
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 12:30:28 -0300

    Se actualiza el codigo usando las constantes

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/util/ApiConfig.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/util/ApiConfig.kt
@@ -1,7 +1,7 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.util
 
 object ApiConfig {
-    const val API_BASE_URL = "https://tuiter.fragua.com.ar"
+    const val API_BASE_URL = "https://tuiter.fragua.com.ar/"
     const val LOCALHOST_BASE_URL = "http://localhost:8000"
     const val APPLICATION_TOKEN_HEADER = "Application-Token"
     const val USER_TOKEN_HEADER = "Authorization"
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/ApiModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/ApiModule.kt
@@ -1,12 +1,13 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import javax.inject.Singleton
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
+import ar.edu.unlam.mobile.scaffolding.data.remote.util.ApiConfig.API_BASE_URL
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,7 +17,7 @@ object ApiModule {
     fun provideRetrofit(): Retrofit =
         Retrofit
             .Builder()
-            .baseUrl("https://tuiter.fragua.com.ar/")
+            .baseUrl(API_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 } // TODO: Se deben agregar providers para cada API
