564ae39bf97b2e9c9ca06ed5250c1e186ffb66cd
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-09 15:46:14 -0300

    DTOs para autenticación

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LoginRequest.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LoginRequest.kt
@@ -1,10 +1,6 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.request
 
-/*
 data class LoginRequest(
-    // TODO: Implementar campos
-)*/
-
-class LoginRequest {
-    // TODO: borrar esta clase y usar la de arriba cuando se implemente
-}
+    val email: String,
+    val password: String,
+)
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/UserResponse.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/response/UserResponse.kt
@@ -1,9 +1,7 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.response
 
-/*data class UserResponse(
-    // TODO: Implementar campos
-)*/
-
-class UserResponse {
-    // TODO: borrar esta clase y usar la de arriba cuando se implemente
-}
+data class UserResponse(
+    val name: String,
+    val email: String,
+    val token: String,
+)
