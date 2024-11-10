85d2ea6a947b135cd3bae68b8691da229aa47744
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-10 18:38:59 -0300

    provide de GetFavoriteUsers, RemoveFavoriteUser y SaveFavoriteUser

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/UseCaseModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/UseCaseModule.kt
@@ -1,6 +1,10 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import ar.edu.unlam.mobile.scaffolding.domain.port.repository.FavoriteUserRepository
 import ar.edu.unlam.mobile.scaffolding.domain.port.repository.ProfileRepository
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.GetFavoriteUsers
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.RemoveFavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.SaveFavoriteUser
 import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.profile.GetProfile
 import dagger.Module
 import dagger.Provides
@@ -12,4 +16,13 @@ import dagger.hilt.components.SingletonComponent
 object UseCaseModule {
     @Provides
     fun provideGetProfile(profileRepository: ProfileRepository): GetProfile = GetProfile(profileRepository)
+
+    @Provides
+    fun provideSaveFavoriteUser(favoriteUserRepository: FavoriteUserRepository): SaveFavoriteUser = SaveFavoriteUser(favoriteUserRepository)
+
+    @Provides
+    fun provideRemoveFavoriteUser(favoriteUserRepository: FavoriteUserRepository): RemoveFavoriteUser = RemoveFavoriteUser(favoriteUserRepository)
+
+    @Provides
+    fun provideGetFavoriteUser(favoriteUserRepository: FavoriteUserRepository): GetFavoriteUsers = GetFavoriteUsers(favoriteUserRepository)
 }
