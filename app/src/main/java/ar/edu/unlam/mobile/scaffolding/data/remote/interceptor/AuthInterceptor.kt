ea7e3a93eda47eab47309540b95dbf2556cd7aae
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-10 16:19:49 -0300

    Implementación de: TokenManager, SharedPreferencesModule y AuthInterceptor para el manejo del token

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/interceptor/AuthInterceptor.kt
@@ -1,5 +1,27 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.interceptor
 
-class AuthInterceptor {
-    // TODO: Implementar interceptor para Application-Token
+import ar.edu.unlam.mobile.scaffolding.data.remote.util.ApiConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class AuthInterceptor @Inject constructor(
+    private val tokenManager: TokenManager
+): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val requestBuilder = request.newBuilder()
+            .header(ApiConfig.APPLICATION_TOKEN_HEADER, ApiConfig.APPLICATION_TOKEN_VALUE)
+
+        tokenManager.getToken()?.let { token ->
+            if (
+                !request.url.encodedPath.contains(ApiConfig.Endpoints.LOGIN)&&
+                !request.url.encodedPath.contains(ApiConfig.Endpoints.REGISTER)
+                ){
+                    requestBuilder.header(ApiConfig.USER_TOKEN_HEADER, token)
+                }
+        }
+        return chain.proceed(requestBuilder.build())
+    }
 }
---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/interceptor/TokenManager.kt
@@ -0,0 +1,30 @@
+package ar.edu.unlam.mobile.scaffolding.data.remote.interceptor
+
+import android.content.SharedPreferences
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TokenManager @Inject constructor(
+    private val sharedPreferences: SharedPreferences
+) {
+    companion object {
+        private const val KEY_TOKEN = "user_token"
+    }
+
+    fun saveToken(token: String) {
+        sharedPreferences.edit()
+            .putString(KEY_TOKEN, token)
+            .apply()
+    }
+
+    fun getToken(): String? {
+        return sharedPreferences.getString(KEY_TOKEN, null)
+    }
+
+    fun clearToken() {
+        sharedPreferences.edit()
+            .remove(KEY_TOKEN)
+            .apply()
+    }
+}
