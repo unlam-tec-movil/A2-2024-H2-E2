672850f4fd758ed81c1c7b049ef39e958cdd1f8a
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 22:54:20 -0300

    ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/CreateTuitRequest.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/CreateTuitRequest.kt
@@ -1,5 +1,5 @@
 package ar.edu.unlam.mobile.scaffolding.data.remote.dto.request
 
 data class CreateTuitRequest(
-    val message: String
+    val message: String,
 )
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/tuit/create/CreateTuitScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/tuit/create/CreateTuitScreen.kt
@@ -8,20 +8,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ar.edu.unlam.mobile.scaffolding.R
-import ar.edu.unlam.mobile.scaffolding.domain.model.Tuit
-import ar.edu.unlam.mobile.scaffolding.domain.port.repository.TuitRepository
-import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.tuit.creation.CreateTuit
 import ar.edu.unlam.mobile.scaffolding.ui.core.component.error.ErrorView
 import ar.edu.unlam.mobile.scaffolding.ui.core.component.loading.LoadingIndicator
-import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
 import ar.edu.unlam.mobile.scaffolding.ui.core.state.onError
 import ar.edu.unlam.mobile.scaffolding.ui.core.state.onLoading
 import ar.edu.unlam.mobile.scaffolding.ui.core.state.onSuccess
-import kotlinx.coroutines.flow.Flow
 
 @Composable
 fun CreateTuitScreen(
@@ -81,7 +75,7 @@ fun CreateTuitScreen(
                 .onLoading {
                     LoadingIndicator()
                 }.onSuccess {
-                    //showSuccessSnackbar = true
+                    showSuccessSnackbar = true
                 }.onError { message ->
                     ErrorView(
                         message = message,
