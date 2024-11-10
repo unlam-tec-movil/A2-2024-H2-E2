ea6e8a7790ad733aaab4125358e71c28355066e3
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-10 18:31:37 -0300

    provide de AppDatabase y FavoriteUserDao

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/DatabaseModule.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/di/DatabaseModule.kt
@@ -1,15 +1,30 @@
 package ar.edu.unlam.mobile.scaffolding.di
 
+import android.content.Context
+import androidx.room.Room
+import ar.edu.unlam.mobile.scaffolding.data.local.AppDatabase
+import ar.edu.unlam.mobile.scaffolding.data.local.dao.FavoriteUserDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-    // TODO: Proveer AppDatabase
-    // TODO: Proveer TuitDao
-    // TODO: Proveer LocalTuitDao
-    // TODO: Proveer UserDao
-    // TODO: Proveer LocalUserDao
+    @Provides
+    @Singleton
+    fun provideAppDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database",
+        ).build()
+    }
+
+    @Provides
+    fun provideFavoriteUserDao(appDatabase: AppDatabase): FavoriteUserDao {
+        return appDatabase.favoriteUserDao()
+    }
 }
