5f0194e45f636c75ca480a0ecee0aa3864e114de
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 22:49:22 -0300

    added imports and toRegisterCredentials

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/AuthMapper.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/mapper/AuthMapper.kt
@@ -2,8 +2,12 @@ package ar.edu.unlam.mobile.scaffolding.data.remote.mapper
 
 import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.ProfileResponse
 import ar.edu.unlam.mobile.scaffolding.data.remote.dto.response.UserResponse
+import ar.edu.unlam.mobile.scaffolding.domain.model.AuthToken
+import ar.edu.unlam.mobile.scaffolding.domain.model.LoginCredentials
+import ar.edu.unlam.mobile.scaffolding.domain.model.RegisterCredentials
 import ar.edu.unlam.mobile.scaffolding.domain.model.User
 import javax.inject.Inject
+
 
 class AuthMapper
     @Inject
@@ -24,6 +28,16 @@ class AuthMapper
             email: String,
             password: String,
         ) = LoginCredentials(
+            email = email,
+            password = password,
+        )
+
+        fun toRegisterCredentials(
+            name: String,
+            email: String,
+            password: String,
+        ) = RegisterCredentials(
+            name: String,
             email = email,
             password = password,
         )
