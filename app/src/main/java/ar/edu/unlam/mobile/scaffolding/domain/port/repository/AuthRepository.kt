5eda5568500e0166f9ad8b64ac81bd9750f7328c
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 22:33:23 -0300

    Se añade un registerCredentials

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/repository/AuthRepository.kt
@@ -1,9 +1,13 @@
 package ar.edu.unlam.mobile.scaffolding.domain.port.repository
 
+import ar.edu.unlam.mobile.scaffolding.domain.model.AuthToken
+import ar.edu.unlam.mobile.scaffolding.domain.model.LoginCredentials
+import ar.edu.unlam.mobile.scaffolding.domain.model.RegisterCredentials
+
 interface AuthRepository {
     suspend fun login(credentials: LoginCredentials): Result<AuthToken>
 
-    suspend fun register(name: String, email: String, password: String): Result<AuthToken>
+    suspend fun register(credentials: RegisterCredentials): Result<AuthToken>
 
     fun saveToken(token: AuthToken)
 
