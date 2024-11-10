1baf3155625ea88d1f8c71db2f48eff5e48e449d
GDevatanian <gonzalo.ocampo.devatanian@hotmail.com>
2024-11-09 22:43:40 -0300

    add imports

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/auth/LoginUser.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/port/usecase/user/auth/LoginUser.kt
@@ -1,5 +1,8 @@
 package ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.auth
 
+import ar.edu.unlam.mobile.scaffolding.domain.model.AuthToken
+import ar.edu.unlam.mobile.scaffolding.domain.model.LoginCredentials
+
 interface LoginUser {
     suspend operator fun invoke(credentials: LoginCredentials): Result<AuthToken>
 }
