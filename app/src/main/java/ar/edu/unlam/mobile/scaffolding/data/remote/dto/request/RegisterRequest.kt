43a51333180879a45a85c4c2685681ce8b37a640
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-10 19:47:12 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/RegisterRequest.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/RegisterRequest.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class RegisterRequest(
     @SerializedName("name") val name: String,
     @SerializedName("email") val email: String,
-    @SerializedName("password") val password: String
+    @SerializedName("password") val password: String,
 )
