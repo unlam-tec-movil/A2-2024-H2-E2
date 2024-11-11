2d6b23dd70a40c5ce2b11864a06e9939c90d534f
Rodrigo Navarro <rodrigoenavarro@hotmail.com>
2024-11-11 20:39:39 -0300

    errores ktlint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/MainActivity.kt
@@ -6,22 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ar.edu.unlam.mobile.scaffolding.ui.components.BottomNavigationBar
-import ar.edu.unlam.mobile.scaffolding.ui.feed.FeedScreen
 import ar.edu.unlam.mobile.scaffolding.ui.navigation.AppNavigation
 import ar.edu.unlam.mobile.scaffolding.ui.navigation.Screen
 import ar.edu.unlam.mobile.scaffolding.ui.theme.ScaffoldingV2Theme
@@ -53,11 +46,12 @@ fun MainScreen() {
 
     val currentRoute = navBackStackEntry?.destination?.route
 
-    val screenWithBottomBar = listOf(
-        Screen.Feed.route,
-        Screen.Profile.route,
-        Screen.FavoriteUsers.route,
-    )
+    val screenWithBottomBar =
+        listOf(
+            Screen.Feed.route,
+            Screen.Profile.route,
+            Screen.FavoriteUsers.route,
+        )
 
     val showBottomBar = currentRoute in screenWithBottomBar
 
@@ -66,13 +60,13 @@ fun MainScreen() {
             if (showBottomBar) {
                 BottomNavigationBar(controller = navController)
             }
-        }
+        },
     ) { paddingValues ->
         Box(
             modifier =
                 Modifier
                     .fillMaxSize()
-                    .padding(paddingValues)
+                    .padding(paddingValues),
         ) {
             AppNavigation(navController = navController)
         }
