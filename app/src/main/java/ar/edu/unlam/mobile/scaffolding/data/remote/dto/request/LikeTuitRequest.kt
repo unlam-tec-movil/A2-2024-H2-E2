c30e80eb54e292841ec54522c1c4bdf2b556cdf9
Juliana Samaniego <jfsamaniego@alumno.unlam.edu.ar>
2024-11-11 20:10:21 -0300

    correcciones klint

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LikeTuitRequest.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/remote/dto/request/LikeTuitRequest.kt
@@ -3,5 +3,5 @@ package ar.edu.unlam.mobile.scaffolding.data.remote.dto.request
 import com.google.gson.annotations.SerializedName
 
 data class LikeTuitRequest(
-    @SerializedName("tuitID") val tuitID: Int
+    @SerializedName("tuitID") val tuitID: Int,
 )
