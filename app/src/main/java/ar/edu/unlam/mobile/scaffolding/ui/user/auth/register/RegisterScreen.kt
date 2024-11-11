ab7eda8429a8857b9d3c5fcde48bdafd32b1e7bc
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-11 14:27:21 -0300

    Implementacion de RegisterScreen y RegisterViewModel

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/auth/register/RegisterScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/auth/register/RegisterScreen.kt
@@ -1,8 +1,132 @@
 package ar.edu.unlam.mobile.scaffolding.ui.user.auth.register
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import ar.edu.unlam.mobile.scaffolding.ui.core.component.error.ErrorView
+import ar.edu.unlam.mobile.scaffolding.ui.core.component.loading.LoadingIndicator
+import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
+import ar.edu.unlam.mobile.scaffolding.ui.core.state.onError
+import ar.edu.unlam.mobile.scaffolding.ui.core.state.onLoading
+import ar.edu.unlam.mobile.scaffolding.ui.core.state.onSuccess
 
 @Composable
-fun RegisterScreen() {
-    // TODO: Implementar pantalla de registro
+fun RegisterScreen(
+    navController: NavController,
+    viewModel: RegisterViewModel = hiltViewModel(),
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    state.registerState
+        .onLoading {
+            LoadingIndicator()
+        }.onSuccess {
+            navController.navigate("home")
+        }.onError {
+            ErrorView(
+                message = it,
+                onRetry = {
+                    viewModel.register()
+                },
+                isRetrying = state.registerState is UIState.Loading,
+            )
+        }
+
+    RegisterForm(
+        name = viewModel.getName(),
+        onNameChange = viewModel::onNameChange,
+        email = viewModel.getEmail(),
+        onEmailChange = viewModel::onEmailChange,
+        password = viewModel.getPassword(),
+        onPasswordChange = viewModel::onPasswordChange,
+        onRegisterClick = viewModel::register,
+    )
+}
+
+@Composable
+fun RegisterForm(
+    name: String,
+    onNameChange: (String) -> Unit,
+    email: String,
+    onEmailChange: (String) -> Unit,
+    password: String,
+    onPasswordChange: (String) -> Unit,
+    onRegisterClick: () -> Unit,
+) {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "Person Icon",
+            modifier = Modifier.size(100.dp),
+            colorResource(
+                id = ar.edu.unlam.mobile.scaffolding.R.color.purple_500,
+            ),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Nombre") },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = email,
+            onValueChange = onEmailChange,
+            label = { Text("Email") },
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = onPasswordChange,
+            label = { Text("Contraseña") },
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = onRegisterClick,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("Registrarse")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterScreenPreview() {
+    RegisterForm(
+        name = "Jane Doe",
+        onNameChange = {},
+        email = "jdoe@example.com",
+        onEmailChange = {},
+        password = "zzz123",
+        onPasswordChange = {},
+        onRegisterClick = {},
+    )
 }
