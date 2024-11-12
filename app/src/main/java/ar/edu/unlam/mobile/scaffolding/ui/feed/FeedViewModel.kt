07d4c7d100b24c5ec75fe0683f4c32635a42b127
nlizarraga9 <nicolaslizarraga152@gmail.com>
2024-11-12 00:33:04 -0300

    se implementa boton para agregar usuarios a favoritos en la TuitCard

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/feed/FeedViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/feed/FeedViewModel.kt
@@ -2,9 +2,13 @@ package ar.edu.unlam.mobile.scaffolding.ui.feed
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import ar.edu.unlam.mobile.scaffolding.domain.model.FavoriteUser
 import ar.edu.unlam.mobile.scaffolding.domain.port.repository.TuitRepository
 import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.tuit.feed.GetFeed
 import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.tuit.feed.RefreshFeed
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.GetFavoriteUsers
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.RemoveFavoriteUser
+import ar.edu.unlam.mobile.scaffolding.domain.port.usecase.user.favorite.SaveFavoriteUser
 import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,12 +23,18 @@ class FeedViewModel
         private val getFeed: GetFeed,
         private val refreshFeed: RefreshFeed,
         private var tuitRepository: TuitRepository,
+        private val saveFavoriteUser: SaveFavoriteUser,
+        private val removeFavoriteUser: RemoveFavoriteUser,
+        private val getFavoriteUsers: GetFavoriteUsers,
     ) : ViewModel() {
         private val _state = MutableStateFlow(FeedState())
         val state = _state.asStateFlow()
 
+        private val favoriteUsers = mutableSetOf<FavoriteUser>()
+
         init {
             loadFeed()
+            loadFavoriteUsers()
         }
 
         private fun loadFeed() {
@@ -43,6 +53,25 @@ class FeedViewModel
                                 UIState.Error(
                                     e.message ?: "Error al cargar los tuits",
                                 ),
+                        )
+                }
+            }
+        }
+
+        private fun loadFavoriteUsers() {
+            viewModelScope.launch {
+                try {
+                    val favoriteUsersList = getFavoriteUsers()
+                    favoriteUsers.clear()
+                    favoriteUsers.addAll(favoriteUsersList)
+                    _state.value =
+                        _state.value.copy(
+                            favoriteUsers = favoriteUsers.toSet(),
+                        )
+                } catch (e: Exception) {
+                    _state.value =
+                        _state.value.copy(
+                            tuitsState = UIState.Error(e.message ?: "Error al cargar los usuarios favoritos"),
                         )
                 }
             }
@@ -95,6 +124,20 @@ class FeedViewModel
             viewModelScope.launch {
                 tuitRepository.unlikeTuit(tuitId = id)
                 // Completar y agregar caso de error
+            }
+        }
+
+        fun onFavoriteClick(favoriteUser: FavoriteUser) {
+            viewModelScope.launch {
+                val isFavorite = favoriteUsers.contains(favoriteUser)
+                if (isFavorite) {
+                    removeFavoriteUser(favoriteUser)
+                    favoriteUsers.remove(favoriteUser)
+                } else {
+                    saveFavoriteUser(favoriteUser)
+                    favoriteUsers.add(favoriteUser)
+                }
+                _state.value = _state.value.copy(favoriteUsers = favoriteUsers.toSet())
             }
         }
     }
