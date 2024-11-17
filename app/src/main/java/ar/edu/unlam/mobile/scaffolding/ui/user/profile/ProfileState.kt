c21e83fa180bc780b15b4b9000a077c249b7c1fb
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-17 16:45:39 -0300

    Update de ProfileScreen, Viewmodel y State para el manejo de edicion de perfil

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileState.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/user/profile/ProfileState.kt
@@ -5,4 +5,6 @@ import ar.edu.unlam.mobile.scaffolding.ui.core.state.UIState
 
 data class ProfileState(
     val profileState: UIState<Profile> = UIState.Loading,
+    val editProfileState: UIState<Unit> = UIState.None,
+    val isEditing: Boolean = false,
 )
