575e3d57448ae3c58929d6a4221f3d393b093229
brenda-rementeria <55760931+brenda-rementeria@users.noreply.github.com>
2024-11-17 16:44:18 -0300

    Implementacion endpoints de edicion de perfil

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/RemoteProfileRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repository/RemoteProfileRepository.kt
@@ -1,6 +1,7 @@
 package ar.edu.unlam.mobile.scaffolding.data.repository
 
 import ar.edu.unlam.mobile.scaffolding.data.remote.api.ProfileApi
+import ar.edu.unlam.mobile.scaffolding.data.remote.dto.request.EditProfileRequest
 import ar.edu.unlam.mobile.scaffolding.data.remote.mapper.ProfileMapper
 import ar.edu.unlam.mobile.scaffolding.domain.model.Profile
 import ar.edu.unlam.mobile.scaffolding.domain.port.repository.ProfileRepository
@@ -14,6 +15,15 @@ class RemoteProfileRepository
     ) : ProfileRepository {
         override suspend fun getProfile(): Profile {
             val response = api.getProfile()
+            return mapper.toDomain(response)
+        }
+
+        override suspend fun updateProfile(
+            name: String,
+            password: String,
+            avatarUrl: String,
+        ): Profile {
+            val response = api.updateProfile(EditProfileRequest(name, password, avatarUrl))
             return mapper.toDomain(response)
         }
     }
